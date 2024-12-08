4ab3defe91456630bb728141af572ca2c2cec806
Jordan Gray <business@jordantgray.uk>
2024-12-08 19:08:09 +0000

    Add all commands

--- Source/FSUIPC/Models/Request/Commands.ts
+++ Source/FSUIPC/Models/Request/Commands.ts
@@ -2,7 +2,25 @@
 export class Commands
 {
     public static AboutRead: string = "about.read";
+
     public static VarsDeclare: string = "vars.declare";
     public static VarsRead: string = "vars.read";
+    public static VarsStop: string = "vars.stop";
+    public static VarsRemove: string = "vars.remove";
+    public static VarsCalc: string = "vars.calc";
+    public static VarsList: string = "vars.list";
+    public static VarsReload: string = "vars.reload";
+    public static VarsWrite: string = "vars.write";
 
+
+    public static OffsetsDeclare: string = "offsets.declare";
+    public static OffsetsRead: string = "offsets.read";
+    public static OffsetsRemove: string = "offsets.remove";
+    public static OffsetsStop: string = "offsets.stop";
+    public static OffsetsWrite: string = "offsets.write";
+
+    public static PayloadAutoload: string = "payload.autoload";
+    public static PayloadRead: string = "payload.read";
+    public static PayloadStop: string = "payload.stop";
+    public static PayloadWrite: string = "payload.write";
 }
